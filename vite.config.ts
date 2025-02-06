680faa664b7b7f47ad29b8e64a4584bf7d04d2a5
Nemchick <92371593+Nemchick@users.noreply.github.com>
2025-02-07 00:31:02 +0600

    ntcn

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/test_Three/", // Замените "my-vue-project" на название вашего репозитория
+  // base: "/test_Three/", // Замените "my-vue-project" на название вашего репозитория
 });
